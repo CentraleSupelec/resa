09592153ce6860f97e7ccc2c6319420130aa3e5d
Benjamin Petit <benjamin.petit@centralesupelec.fr>
2023-12-11 12:10:31 +0100

    feat(ibm) 🔧 add IBM rooms + add IBM filter + remove Lumen filter
    GitOrigin-RevId: 8fc6ce1538cc961eec4bab1084a2f11567b92550

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -27,6 +27,6 @@ const loginQuery = querystring.stringify({
 export const loginRequest = `${config.cas.loginUrl}?${loginQuery}`;
 export const CAMPUSES = ['Saclay', 'Metz', 'Rennes'];
 export const CAMPUS_SACLAY = 'Saclay';
-export const BUILDINGS = ['Falguière', 'Lumen'];
+export const BUILDINGS = ['Falguière', 'IBM'];
 
 export default config;
